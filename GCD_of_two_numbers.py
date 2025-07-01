4c10c85217e88ee3f895c9519faafe50eb0a4978
HasmithaPolamuri <162907869+HasmithaPolamuri@users.noreply.github.com>
2025-07-01 11:23:18 +0530

    Update GCD_of_two_numbers.py

--- GCD_of_two_numbers.py
+++ GCD_of_two_numbers.py
@@ -1,6 +1,6 @@
 def gcd(m,n):
-  cf=[]
-  for i in range(1,min(m+n)+1):
+  cf=[]      #List of common factors
+  for i in range(1,min(m+n)+1):     #gcd is less than or equal to smaller number
     if ((m%i)==0 and (m%i)==0):
       cf.append(i)
-  return(cf[-1])
+  return(cf[-1])  #returns the highest common factor
