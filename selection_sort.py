c9e864ef55ab5ba9ad54f56396bec1aebdf818eb
HasmithaPolamuri <162907869+HasmithaPolamuri@users.noreply.github.com>
2025-07-07 12:21:58 +0530

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,25 @@
+'''
+Find the smallest element in the unsorted part of the list.
+Swap it with the first element of the unsorted part.
+Move the boundary of sorted/unsorted part one step forward.
+Repeat until the list is sorted.
+
+Best Case - O(n)
+Average Case - O(n²)
+Worst Case - O(n²)
+
+Stable - Yes
+Sort in Place - Yes
+'''
+
+def selectionsort(L):
+    n = len(L)
+    if n < 1:
+        return(L)
+    for i in range(n):
+        minpos = i
+        for j in range(i+1,n):
+            if L[j] < L[minpos]:
+                minpos = j
+        (L[i],L[minpos]) = (L[minpos],L[i])
+    return(L)
